30277852733b2d56106486a0b19a9158d2d3941a
Wael Mahrous <waelbmahrous@gmail.com>
2022-03-31 14:50:05 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/misc")
+@app.route("/api/server")
 def hello_world():
-    return {"message": "Hello World!"}
+
+    client = MongoClient('mongodb+srv://whaam:B-oop123@project2022.yskak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    db = client.get_database("configuration")
+    collection = db.get_collection("setup")
+
+    doc = collection.find_one({"name": "setup"})
+    print(doc)
+
+    return {"status": doc["status"]}
+
+# Run the app
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
