567243ff0344cc587b4fb081d65e1940b1ef3466
waelsucks <waelbmahrous@gmail.com>
2022-05-04 15:50:08 +0200

    Working

--- app.py
+++ app.py
@@ -6,31 +6,9 @@ app = Flask(__name__)
 CORS(app)
 
 client = MongoClient(
-    'mongodb+srv://whaam:B-oop123@project2022.yskak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    'mongodb+srv://whaam:B-oop123@project2022.yskak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true')
 
-
-@app.route("/api/server")
-def hello_world():
-
-    db = client.get_database("configuration")
-    collection = db.get_collection("setup")
-
-    doc = collection.find_one({"name": "setup"})
-    print(doc)
-
-    return {"status": doc["status"]}
-
-
-@app.route("/api/get_movement")
-def get_movement():
-
-    db = client.get_database("configuration")
-    collection = db.get_collection("setup")
-
-    # collection.insert_one({"name": "movement", "status": ""})
-    doc = collection.find_one({"name": "movement"})
-
-    return {"status": doc["status"]}
+# Registers a new user
 
 
 @app.route("/api/start_game", methods=["POST"])
@@ -43,25 +21,46 @@ def start_game():
         {"name": "setup"}, {"$set": {"username": request.json["username"],
                                      "status": True}})
 
-    # print(request.json["username"])
-
     return {"status": True}
 
+# Shows player status
 
-@app.route("/api/games_played")
-def games_played():
+
+@app.route("/api/get_game_status", methods=["POST"])
+def get_game_status():
+
+    db = client.get_database("configuration")
+    collection = db.get_collection("players")
+
+    doc = collection.find_one({"username": request.json["username"]})
+
+    if doc is None:
+        collection.insert_one({"username": request.json["username"],
+                                 "status": "not playing"})
+    
+    doc = collection.find_one({"username": request.json["username"]})
+
+    return {"status": doc["status"]}
+
+# Showing the game scoreboard
+
+
+@app.route("/api/get_scoreboard")
+def get_scoreboard():
 
     db = client.get_database("stats")
-    collection = db.get_collection("games")
+    collection = db.get_collection("scoreboard")
 
     docs = []
 
-    for doc in collection.find({}, {"_id": 0}).sort("score", 1).limit(5):
+    for doc in collection.find({}, {"_id": 0}).sort("score", 1).limit(10):
         docs.append(doc)
 
     return {"games": docs}
 
 
 # Run the app
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
